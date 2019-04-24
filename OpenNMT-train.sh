4f0f8d9d541fbb85d9e83ff3ad12a8dd4daa18c1
adrianpearl <adrianspearl@gmail.com>
2019-04-23 22:10:59 -0400

    virtualenv

--- OpenNMT-train.sh
+++ OpenNMT-train.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-#SBATCH -N 1
 #SBATCH -n 1
+#SBATCH --tasks-per-node 1
+#SBATCH --time 48:00:00
 #SBATCH —gres=gpu:1
 #SBATCH —mem=8GB
 #SBATCH —mail-user=egs345@nyu.edu
 
-module load python3/intel/3.6.3
 python OpenNMT-py/train.py -save_model demo_model \
-                -data data/CNNDM \
+                -data ../data/CNNDM \
                 -train_steps 100 \
                 -global_attention mlp \
                 -word_vec_size 128 \
