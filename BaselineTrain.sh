41df53fbae36cdde8857578b392d6b547d48ae7f
elliotgsilva <37222555+elliotgsilva@users.noreply.github.com>
2019-05-02 17:00:37 -0400

    increasing mem requested

--- BaselineTrain.sh
+++ BaselineTrain.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=48:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:p40:1
-#SBATCH --mem=32GB
+#SBATCH --mem=40GB
 
 python OpenNMT-py/train.py -save_model model_full \
     -data ../data/CNNDM \
