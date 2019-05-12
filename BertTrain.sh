867264cedb51a31363dfaad5966c4a0812dafea9
adrianpearl <adrianspearl@gmail.com>
2019-05-12 16:42:31 -0400

    reduced batch size

--- BertTrain.sh
+++ BertTrain.sh
@@ -2,17 +2,19 @@
 #SBATCH -n 1
 #SBATCH --tasks-per-node=1
 #SBATCH --time=48:00:00
+#SBATCH --gres=gpu:p1080:1
 #SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:p40:1
-#SBATCH --mem=64GB
+#SBATCH --mem=4GB
 
 python OpenNMT-py/train.py -save_model model_Bert \
-    -data data_full/split/BERT/BERT \
-    -train_steps 1000 \
+    -data ../../data_full/split/BERT/BERT \
+    -train_steps 125000 \
     -encoder_type bert \
     -layers 1 \
     -rnn_size 768 \
-    -batch_size 64 \
-    -valid_batch_size 64 \
+    -batch_size 32 \
+    -valid_batch_size 32 \
     -learning_rate 0.02 \
-    -bert_vocab data_full/split/src_tok/src-vocab.vcb
+    -gpu_ranks 0 \
+    -save_checkpoint_steps 8000 \
+    -bert_vocab ../../data_full/split/src_tok/src-vocab.vcb
